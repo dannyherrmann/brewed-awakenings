c21bf2665d815b2dead4cd29b37d50e764ed1c38
Danny Herrmann <dannyherrmann91@gmail.com>
2022-09-14 21:09:45 -0500

    uploading

---
+++ scripts/Orders.js
@@ -0,0 +1,76 @@
+import { getProducts, getEmployees, getOrders } from "./database.js"
+
+// Get copy of state for use in this module
+const products = getProducts()
+const employees = getEmployees()
+const orders = getOrders()
+
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+    let orderProduct = null
+    for (const product of allProducts) {
+        if (product.id === order.productId) {
+            orderProduct = product
+        }
+    }
+    return orderProduct
+}
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+    let orderEmployee = null
+    for (const employee of allEmployees) {
+        if (employee.id === order.employeeId) {
+            orderEmployee = employee
+        }
+    }
+
+    return orderEmployee
+}
+
+export const Orders = () => {
+    let html = ""
+    html = "<ul>"
+
+    for (const order of orders) {
+        const employee = findEmployee(order, employees)
+        const product = findProduct(order, products)
+        html += `<li>${product.name} was sold by ${employee?.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+const employeeOrders = (id) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === id) {
+            fulfilledOrders += 1
+        }
+    } if (fulfilledOrders === 1) {
+            return `${fulfilledOrders} product`
+        } else {
+            return `${fulfilledOrders} products`}
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const employeeId = parseInt(itemClicked.id.split("--")[1])
+            const orderCount = employeeOrders(employeeId)
+            for (const employee of employees) {
+                if (employee.id === employeeId) {
+                    window.alert(`${employee.name} sold ${orderCount}`)
+                }
+            }
+        }
+    }
+)
+
+
